99b3cd5ea4c52bc50d722355226aca05ec66c419
Atw <35207756+Atww@users.noreply.github.com>
2021-06-08 14:20:54 +0700

    :flags:  Websocket StreamName

--- src/ws_bitkub.controller.ts
+++ src/ws_bitkub.controller.ts
@@ -1,6 +1,5 @@
-import { on } from "ws";
 import { BITKUB_URL_WS } from './util/constant';
-
+import WebSocket from 'ws'
 class WebSocketBitkubController {
     private serviceName: string
     private serviceType: string
@@ -8,16 +7,29 @@ class WebSocketBitkubController {
     private ws: WebSocket
     constructor() {
     }
-    public setStreamName(serviceName: string, serviceType: string, symbol: string): void {
+    public setFullStreamName(serviceName: string, serviceType: string, symbol: string): void {
         this.serviceName = serviceName;
         this.serviceType = serviceType;
         this.symbol = symbol;
-        this.ws = new WebSocket(`${BITKUB_URL_WS}/${serviceName}.${serviceType}.${symbol}`)
     }
-    public getWs() {
-        return this.ws;
+    public setServiceName(serviceName: string): void {
+        this.serviceName = serviceName;
     }
-
+    public setServiceType(serviceType: string): void {
+        this.serviceType = serviceType;
+    }
+    public setSymbol(symbol: string): void {
+        this.symbol = symbol;
+    }
+    public getStreamName() :string {
+        return `${this.serviceName}.${this.serviceType}.${this.symbol}`;
+    }
+    public initWebSocket():void{
+        this.ws = new WebSocket(`${BITKUB_URL_WS}/${this.serviceName}.${this.serviceType}.${this.symbol}`)
+    }
+    public getWebSocket():WebSocket{
+        return this.ws
+    }
 }
 
 export default WebSocketBitkubController
